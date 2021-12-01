b9aa8f1e9c6047dab0b87675e83c4c37693de803
Pavel <pavel.official@bk.ru>
2021-12-02 02:32:59 +0300

    task1 done

--- Lesson_1/Lesson_1/MainWindow.xaml.cs
+++ Lesson_1/Lesson_1/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lesson_1
 {
@@ -20,9 +9,54 @@ namespace Lesson_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread _thread;
+        private int _fibNumb = 0;
+        private int _num;
+
+        /// <summary> Число Фибоначчи. </summary>
+        public int Numb { get; set; }
+
+        /// <summary> Порядковый номер числа Фибоначчи. </summary>
+        public int FibNumb
+        {
+            get => _fibNumb;
+            set
+            {
+                if(int.TryParse(value.ToString(), out _num) && value > 0 && value <= 35)
+                {
+                    _fibNumb = value;
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        /// <summary> Событие нажатия на кнопку. </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _thread = new Thread(() => { Numb = Fibonachi(_fibNumb); });
+
+            _thread.Start();
+            tb.Text = Numb.ToString();
+        }
+
+        /// <summary> Вычислить число Фибоначчи по порядковому номеру. </summary>
+        /// <param name="n"> Порядковый номер числа. </param>
+        /// <returns> Число Фибоначчи. </returns>
+        private int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
         }
     }
 }
