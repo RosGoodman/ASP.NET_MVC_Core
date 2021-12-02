f87c78da867163246e1d588a466eb6272763dcef
RosGoodman <pavel.official@bk.ru>
2021-12-02 12:35:47 +0300

    Task 3 done

--- Lesson_1/Lesson_1/MainWindow.xaml.cs
+++ Lesson_1/Lesson_1/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Lesson_1
 {
@@ -10,8 +14,20 @@ namespace Lesson_1
     public partial class MainWindow : Window
     {
         private Thread _thread;
-        private int _fibNumb = 0;
+        private int _fibCount = 1;
+        private int _regulator = 1;
+
+        private List<int> _numbs;
         private int _num;
+        
+        private object _locker = new object();
+
+        /// <summary> Регулятор кол-ва секунд. </summary>
+        public int Regulator
+        {
+            get => _regulator / 1000;
+            set => _regulator = value * 1000;
+        }
 
         /// <summary> Число Фибоначчи. </summary>
         public int Numb { get; set; }
@@ -19,12 +35,12 @@ namespace Lesson_1
         /// <summary> Порядковый номер числа Фибоначчи. </summary>
         public int FibNumb
         {
-            get => _fibNumb;
+            get => _fibCount;
             set
             {
                 if(int.TryParse(value.ToString(), out _num) && value > 0 && value <= 35)
                 {
-                    _fibNumb = value;
+                    _fibCount = value;
                 }
             }
         }
@@ -40,10 +56,28 @@ namespace Lesson_1
         /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            _thread = new Thread(() => { Numb = Fibonachi(_fibNumb); });
+            _numbs = new List<int>();
+            for (int i = 1; i < _fibCount; i++)
+            {
+                _numbs.Add(Fibonachi(i));
+            }
+             
+            _thread = new Thread(() => 
+            {
+                foreach (var n in _numbs)
+                {
+                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, () =>
+                        {
+                            tb.Text = n.ToString();
+                            Thread.Sleep(_regulator);
 
+                            //Task 3
+                            //Thread.ResetAbort(); //System.PlatformNotSupportedException: "Thread abort is not supported on this platform."
+                        });
+                }
+            });
             _thread.Start();
-            tb.Text = Numb.ToString();
+            _thread.Join();
         }
 
         /// <summary> Вычислить число Фибоначчи по порядковому номеру. </summary>
