1ab9027cf0011dbe4a9550e9cf5a1ea2ca07d96d
RosGoodman <pavel.official@bk.ru>
2021-12-06 11:20:39 +0300

    Lesson_2 done

--- Lesson_2/Lesson_2/Program.cs
+++ Lesson_2/Lesson_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lesson_2;
+
+public static class Program
+{
+    static void Main()
+    {
+        using (var pool = new SimpleThreadPool(5))
+        {
+            var random = new Random();
+            Action<int> randomizer = (index =>
+            {
+                Console.WriteLine("{0}: Working on index {1}", Thread.CurrentThread.Name, index);
+                Thread.Sleep(random.Next(20, 400));
+                Console.WriteLine("{0}: Ending {1}", Thread.CurrentThread.Name, index);
+            });
+
+            for (var i = 0; i < 40; ++i)
+            {
+                var i1 = i;
+                pool.QueueTask(() => randomizer(i1));
+            }
+        }
+    }
+}
